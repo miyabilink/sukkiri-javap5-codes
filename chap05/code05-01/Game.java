935b02619697a2cf40d6b0d4940b5021f37dfc1d
Takuma SUGAWARA <sugawara@miyabilink.jp>
2026-04-14 20:20:53 +0900

    5章

---
+++ chap05/code05-01/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+  void main() {
+    IO.println("RPG: スッキリ魔王征伐 ver0.2");
+    Hero h = new Hero();
+    /* : */
+  }
+}
---
+++ chap05/code05-04/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  void main() {
+    Bank b1 = new Bank();
+    b1.name = "ミナト";
+    b1.address = "東京都";
+    IO.println(b1.hashCode());
+  }
+}
